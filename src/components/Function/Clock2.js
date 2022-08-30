ea9724ceb415d61b57e376390f925cd62564ef0e
riandroraul <riandroraul@gmail.com>
2022-08-30 16:47:25 +0700

    first commit

--- src/components/Function/Clock2.js
+++ src/components/Function/Clock2.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 
 const Clock2 = () => {
-  const [time, setTime] = useState({ date: new Date() });
+  const [time, setTime] = useState({ date: new Date(), date2: new Date() });
   const tick = () => {
-    setTime({ date: new Date() });
+    setTime({ date: new Date(), date2: new Date() });
   };
 
   useEffect(() => {
@@ -12,10 +12,20 @@ const Clock2 = () => {
     }, 1000);
   }, [time]);
 
+  const { date, date2 } = time;
   return (
     <div>
       <h1>Hello, dunia!</h1>
-      <h2>Ini {time.date.toLocaleTimeString()}</h2>
+      <h2>
+        Hari ini :{"   "}
+        {date2.toLocaleDateString(undefined, {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        })}
+      </h2>
+      <h2>jam : {date.toLocaleTimeString("en-GB")}</h2>
     </div>
   );
 };
